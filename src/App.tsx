54207f5528fe22e71b279da51d816cac0418a7c8
aryanm9026 <aryanmishra010@gmail.com>
2025-09-30 01:54:35 +0530

    Initial commit in branch by Aryan

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import StudentDashboard from "./pages/StudentDashboard";
+import AdminPanel from "./pages/AdminPanel";
+import NotFound from "./pages/NotFound";
+import StudentLogin from "./pages/StudentLogin";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/student/dashboard" element={<StudentDashboard />} />
+          <Route path="/admin/panel" element={<AdminPanel />} />
+          <Route path="/student/login" element={<StudentLogin />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
